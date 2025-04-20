8b62607824427e84aed007e91bbd466791e31bea
XhemiT <xhemile.tarko1@gmail.com>
2025-04-20 22:41:59 +0200

    Update project files

--- obj/Debug/net9.0/FlightApi.AssemblyInfo.cs
+++ obj/Debug/net9.0/FlightApi.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("FlightApi")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+b91b12f36f8db23ac099d6770c0586bf7ae46fa7")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+3767a73294a50df6fdd9ed11f9c7cb6b1306318a")]
 [assembly: System.Reflection.AssemblyProductAttribute("FlightApi")]
 [assembly: System.Reflection.AssemblyTitleAttribute("FlightApi")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
